58b1ecd4005f09e7abff6b8e9d9a05d6cbd551aa
Kawish Zia <kawishzia2826@gmail.com>
2024-11-28 10:58:14 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// var itemsArray = [
+//   {name: "juice", price: "50", quantity: "3"},
+//   {name: "cookie", price: "30", quantity: "9"},
+//   {name: "shirt", price: "880", quantity: "1"},
+//   {name: "pen", price: "100", quantity: "2"}
+// ];
+
+// let total = 0; 
+
+// for (let i = 0; i < itemsArray.length; i++) {
+//   console.log(itemsArray[i].price * itemsArray[i].quantity);
+//   total += itemsArray[i].price * itemsArray[i].quantity; 
+// }
+
+// console.log("Total:", total); // Output the total
+
+// let user = {
+//   name: "John Doe",
+//   email: "john.doe@example.com",
+//   password: "securepassword123",
+//   age: 30,
+//   gender: "Male",
+//   city: "New York",
+//   country: "USA"
+// };
+
+// if ('age' in user) {
+//   console.log(" exist in object");
+  
+// } else{
+//   console.log(" does not exist in object");
+  
+// }
+
+// if ('age' in user) {
+//   console.log(" exist in object");
+  
+// } else{
+//   console.log(" does not exist in object");
+  
+// }
+// if ('country' in user) {
+//   console.log(" country exist in object");
+  
+// } else{
+//   console.log(" country does not exist in object");
+  
+// }
+// if ('name' in user) {
+//   console.log( " name exist in object");
+  
+// } else{
+//   console.log(" name does not exist in object");
+  
+// }
+// if ('nalastNameme' in user) {
+//   console.log( " lastName name exist in object");
+  
+// } else{
+//   console.log("lastName name does not exist in object");
+  
+// }
+
+
+// // Create a constructor function with some properties. Now
+// // create multiple records using the constructor.
+
+// function User(name, email, password, age, gender, city, country) {
+//   this.name = name;
+//   this.email = email;
+//   this.password = password;
+//   this.age = age;
+//   this.gender = gender;
+//   this.city = city;
+//   this.country = country;
+// }
+
+// const user1 = new User("John Doe", "john.doe@example.com", "securepassword123", 30, "Male", "New York", "USA");
+// const user2 = new User("kawish Zia","kawihszia2826@gmail.com","1234456","19","Male","karachi","pakistan")
+
+// console.log(user2);
+
+var c = document.getElementsByName("gender")
+
+function myFunction() {
+  var x = document.getElementById("mySelect").value;
+
+
+  
+}
+  
+  
+function information(){
+//   var a =document.getElementById("name")
+// var b = document.getElementById("address")
+//     // if (c[0].checked) {
+//     //    document.getElementsByName("gender")[0].value
+//     // } else {
+//     //    document.getElementsByName("gender")[1].value
+//     // }
+//     function Records(name,addres) {
+//       this.name = name;
+//       this.addres = addres;
+//       }
+
+
+//       console.log(new Records(a,b));
+console.log("hello world");
+
+  }
+
+  
